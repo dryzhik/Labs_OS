b79890e8acf7ec149e0217a7218a4e8f167fc150
daryar679 <daryar@router.lab209.kaf>
2021-09-29 15:43:38 +0300

    Working version

--- lab_1/lab1.c
+++ lab_1/lab1.c
@@ -4,7 +4,40 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
-int main(int argc, char** argv)
+void exit_msg()
 {
+	printf("Atexit handler has worked from process %d\n", getpid());
+}
+
+int main(int argc, char** argv)
+{	
+	atexit(exit_msg);
+	
+	int status = 0;
+		
+	pid_t pid = fork();
+	switch(pid)
+	{
+		case -1:
+		     
+		     perror("fork");
+		     exit(1);
+		
+		case 0:
+		     
+		     printf("CHILD PROCESS: Hi! I'm child.\n");
+		     printf("CHILD PROCESS: My pid is: %d\n", getpid());     
+		     printf("CHILD PROCESS: My parent's pid is: %d\n", getppid());
+		     sleep(10);
+		     exit(1);
+
+		default:
+		    
+		     printf("PARENT PROCESS: Hi! I'm parent.\n");
+		     printf("PARENT PROCESS: My  pid is: %d\n", getpid());
+		     printf("PARENT'S CHILD PROCESS: My child's pid is: %d\n", pid);
+		     wait(&status);
+	}
+	
 	return 0;
 }

c1dc501f64cfe69ac563bce242ee25c242b849e3
dryzhik <91097017+dryzhik@users.noreply.github.com>
2021-10-05 17:22:33 +0300

    Added comments

--- lab_1/lab1.c
+++ lab_1/lab1.c
@@ -11,31 +11,33 @@ void exit_msg()
 
 int main(int argc, char** argv)
 {	
-	atexit(exit_msg);
+	atexit(exit_msg); //регистрирует функцию exit_msg() при нормальном завершении процесса(с помощью системного вызова exit() или возврата результатов функции main())
 	
 	int status = 0;
 		
-	pid_t pid = fork();
+	pid_t pid = fork(); //системный вызов для создания нового процесса(ветвления)
 	switch(pid)
 	{
-		case -1:
+		case -1:// в вслучае ошибки fork() возвращает -1, дочерний процесс не создается
 		     
 		     perror("fork");
 		     exit(1);
 		
-		case 0:
+		case 0: //успешный запуск  fork() возвращает 0, запускается новый(дочерний) процесс
 		     
 		     printf("CHILD PROCESS: Hi! I'm child.\n");
-		     printf("CHILD PROCESS: My pid is: %d\n", getpid());     
-		     printf("CHILD PROCESS: My parent's pid is: %d\n", getppid());
-		     exit(1);
+		     printf("CHILD PROCESS: My pid is: %d\n", getpid());          //getpid() возвращает идентификатор вызывающего процесса (не может возвращать ошибку)
+		     printf("CHILD PROCESS: My parent's pid is: %d\n", getppid());//getppid() возвращает идентификатор родителя вызывающего процесса(не может возвращать ошибку)
+		     exit(1);// завершение процесса, перед завершением обращается к  функции atexit()
 
 		default:
 		    
 		     printf("PARENT PROCESS: Hi! I'm parent.\n");
 		     printf("PARENT PROCESS: My  pid is: %d\n", getpid());
 		     printf("PARENT'S CHILD PROCESS: My child's pid is: %d\n", pid);
-		     wait(&status);
+		     wait(&status);//возвращает pid завершенного дочернего процесса или –1 в случае ошибки
+				   //если никакого дочернего процесса не было прервано, то вызов блокируется, пока потомок не завершится
+				   //если дочерний процесс уже был завершен, то результат возвращается немедленно
 	}
 	
 	return 0;
